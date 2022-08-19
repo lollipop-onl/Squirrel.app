b8f6b6873da3e94346ffae4b12ebccc950fea1b1
lollipop-onl <lollipop@simochee.net>
2022-08-19 16:46:11 +0900

    ⚡️ update status bar

--- src/components/AppStatusBar.tsx
+++ src/components/AppStatusBar.tsx
@@ -1,14 +1,21 @@
 import React from "react";
 import { ButtonReload } from "~/components/ButtonReload";
+import { IonIcon } from "@ionic/react";
+import { reloadOutline } from "ionicons/icons";
 
 type Props = {};
 
 export const AppStatusBar: React.FC<Props> = () => {
   return (
     <div className="flex h-12 w-full items-center bg-black px-4">
-      <ButtonReload>
-        <span className="text-white">reload</span>
-      </ButtonReload>
+      <div className="text-xs font-bold text-white">Squirrel.app</div>
+      <div className="ml-auto">
+        <ButtonReload>
+          <span className="text-white">
+            <IonIcon icon={reloadOutline} />
+          </span>
+        </ButtonReload>
+      </div>
     </div>
   );
 };
